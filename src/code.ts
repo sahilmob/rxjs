d71fef103beb5e9222cfe5a3e465bf5be8559650
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-03 02:22:54 +0300

    Create observable and two observers

--- src/code.ts
+++ src/code.ts
@@ -1,2 +1,39 @@
-import { Observable as Rx } from "rxjs";
-console.log(Rx);
+import { Observable } from "rxjs";
+
+var observable = Observable.create((observer:any) =>{
+try {
+    observer.next('Hey guys!')
+    observer.next('How are you?')
+    setInterval(() => {
+        observer.next('I am good')
+    }, 2000)
+}catch(err) {
+    observer.error(err)
+}
+
+});
+
+var observer = observable.subscribe(
+    (x:any) => addItem(x),
+    (error:any) => addItem(error),
+    (complete:any) => addItem("Completed"),
+);
+
+
+
+var observer2 = observable.subscribe(
+    (x:any) => addItem(x),
+);
+
+observer.add(observer2);
+
+setTimeout(()=>{
+    observer.unsubscribe();
+}, 6001);
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
